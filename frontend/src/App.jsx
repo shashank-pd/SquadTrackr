f169a6bf0f93e4b4daad61d36200a4d8dd5fb468
Ayush Singh <asayushsingh80@gmail.com>
2025-05-01 16:13:41 +0530

    react router + css setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LandingPage from "./pages/LandingPage";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>hello</div>
-    </>
+    <Router>
+      <div className="app-container">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
